6d020cbc96acbf6f6b5142509e090f7ea7ac5a4d
bbpandc <haeminsia@gmail.com>
2024-10-11 15:44:15 +0900

    사용자 정보 수정 기능 추가

--- src/main/java/com/project/doctor_fish_back/controller/UserController.java
+++ src/main/java/com/project/doctor_fish_back/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.project.doctor_fish_back.controller;
 
+import com.project.doctor_fish_back.aspect.annotation.ValidAop;
 import com.project.doctor_fish_back.dto.request.user.ReqModifyUserDto;
+import com.project.doctor_fish_back.dto.request.user.ReqModifyUserEmailDto;
+import com.project.doctor_fish_back.dto.request.user.ReqModifyUserPasswordDto;
 import com.project.doctor_fish_back.exception.AuthorityException;
 import com.project.doctor_fish_back.security.principal.PrincipalUser;
 import com.project.doctor_fish_back.service.UserService;
@@ -8,7 +11,10 @@ import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
@@ -23,10 +29,25 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getUserInfo(principalUser.getId()));
     }
 
-    // 사용자 정보 수정
-    @PutMapping("/user")
-    public ResponseEntity<?> modifyUser(@RequestBody ReqModifyUserDto dto) {
-        return ResponseEntity.ok().body(null);
+    // 사용자 정보 수정(이름, 전화번호, 프로필사진)
+    @ValidAop
+    @PutMapping("/user/{userId}")
+    public ResponseEntity<?> modifyUser(@PathVariable Long userId, @Valid @RequestBody ReqModifyUserDto dto, BindingResult bindingResult) throws NotFoundException, AuthorityException {
+        return ResponseEntity.ok().body(userService.modifyUser(userId, dto));
+    }
+
+    // 사용자 이메일 변경
+    @ValidAop
+    @PutMapping("/user/{userId}/email")
+    public ResponseEntity<?> modifyUserEmail(@PathVariable Long userId, @Valid @RequestBody ReqModifyUserEmailDto dto, BindingResult bindingResult) throws NotFoundException, AuthorityException {
+        return ResponseEntity.ok().body(userService.modifyUserEmail(userId, dto));
+    }
+
+    // 사용자 비밀번호 변경
+    @ValidAop
+    @PutMapping("/user/{userId}/password")
+    public ResponseEntity<?> modifyUserPassword(@PathVariable Long userId, @Valid @RequestBody ReqModifyUserPasswordDto dto, BindingResult bindingResult) throws NotFoundException, AuthorityException {
+        return ResponseEntity.ok().body(userService.modifyUserPassword(userId, dto));
     }
 
     // 회원 탈퇴
