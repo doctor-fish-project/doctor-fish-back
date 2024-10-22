8c88ff7fc78ba23c71ddf4d8b8f69324f6012c7d
bbpandc <haeminsia@gmail.com>
2024-10-22 09:49:22 +0900

    카테고리 가져오기

--- src/main/java/com/project/doctor_fish_back/repository/user/RoleMapper.java
+++ src/main/java/com/project/doctor_fish_back/repository/user/RoleMapper.java
@@ -4,7 +4,7 @@ import com.project.doctor_fish_back.entity.Role;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface UserRoleMapper {
+public interface RoleMapper {
     Role findByPosition(String position);
     int save(Role role);
 
